1a79dc1feb7e037a32ac34b8ae7b1f2375eb48da
HuySG <huybc2011@gmail.com>
2024-04-17 13:46:06 +0700

    update transaction

--- EPNCNL/lib/widgets/custom_phone_number.dart
+++ EPNCNL/lib/widgets/custom_phone_number.dart
@@ -12,11 +12,10 @@ class CustomPhoneNumber extends StatelessWidget {
     required this.onTap,
     required this.controller,
     required this.text,
-    required this.validator,
   }) : super(
           key: key,
         );
-  final String? Function(String, Country) validator;
+
   Country country;
 
   Function(Country) onTap;
@@ -86,7 +85,6 @@ class CustomPhoneNumber extends StatelessWidget {
                 controller: controller,
                 hintText: text,
                 hintStyle: CustomTextStyles.titleSmallBlack900,
-                validator: (value) => validator!(value!, country),
               ),
             ),
           ),

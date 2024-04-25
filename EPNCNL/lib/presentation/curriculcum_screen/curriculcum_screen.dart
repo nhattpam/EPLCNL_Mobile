4e991b87d1da15e3dd59ed668219cd47ca4ea0a4
HuySG <huybc2011@gmail.com>
2024-04-25 11:53:36 +0700

    display class online

--- EPNCNL/lib/presentation/curriculcum_screen/curriculcum_screen.dart
+++ EPNCNL/lib/presentation/curriculcum_screen/curriculcum_screen.dart
@@ -16,13 +16,11 @@ import 'package:meowlish/presentation/curriculcum_screen/widgets/videoplayer_wid
 import 'package:meowlish/presentation/doing_assignment_screen/doing_assignment_screen.dart';
 import 'package:meowlish/presentation/doing_quiz_screen/doing_quiz_screen.dart';
 import 'package:meowlish/presentation/home_page/home_page.dart';
-import 'package:meowlish/presentation/home_page/search/search.dart';
 import 'package:meowlish/presentation/indox_chats_page/indox_chats_page.dart';
 import 'package:meowlish/presentation/my_course_completed_page/my_course_completed_page.dart';
 import 'package:meowlish/presentation/profiles_page/profiles_page.dart';
-import 'package:meowlish/presentation/review_assignment_screen/review_assignment_screen.dart';
 import 'package:meowlish/presentation/transactions_page/transactions_page.dart';
-import 'package:meowlish/session/session.dart';
+import 'package:meowlish/presentation/view_all_assignment_attemp/view_all_assignment_attemp.dart';
 
 class CurriculumScreen extends StatefulWidget {
   final courseID;
@@ -49,7 +47,6 @@ class CurriculumScreenState extends State<CurriculumScreen> {
   Map<String, List<Quiz>> moduleQuizMap = {};
   Map<String, List<Assignment>> moduleAssignmentMap = {};
   Map<String, List<AssignmentAttempt>> moduleUngradeAssignmentAttempt = {};
-  Map<String, List<AssignmentAttempt>> moduleUndoAssignmentAttempt = {};
 
   // Maps to track minimized states for each type of content within each module
   Map<String, bool> minimizedLessonsMap = {};
@@ -83,7 +80,6 @@ class CurriculumScreenState extends State<CurriculumScreen> {
     try {
       List<Module> loadedModule =
       await Network.getModulesByCourseId(widget.courseID);
-      loadedModule.sort((a, b) => (b.createdDate.toString()).compareTo(a.createdDate.toString()));
       setState(() {
         listModuleByCourseId = loadedModule;
         isLoadingModule = false;
@@ -162,7 +158,6 @@ class CurriculumScreenState extends State<CurriculumScreen> {
         // Store the lessons for this module in the map
         moduleAssignmentMap[moduleId] = loadedAssignment;
         for(var assignment in (moduleAssignmentMap[moduleId] as List) ){
-          loadAssignmentAttemptByLearnerId(assignment.id.toString());
           loadAssignmentAttemptByAssignmentId(assignment.id.toString());
         }
         isLoadingAssignment = false;
@@ -194,19 +189,6 @@ class CurriculumScreenState extends State<CurriculumScreen> {
 
       setState(() {
         moduleUngradeAssignmentAttempt[assignmentId] = assignment;
-        // Add more print statements for other properties if needed
-      });
-    } catch (e) {
-      // Handle errors here
-      print('Error: $e');
-    }
-  }
-  Future<void> loadAssignmentAttemptByLearnerId(String assignmentId) async {
-    final lid = SessionManager().getLearnerId();
-    try {
-      final assignment = await FetchCourseList.getPeerReviewByLearnerId(assignmentId: assignmentId, query: lid);
-      setState(() {
-        moduleUndoAssignmentAttempt[assignmentId] = assignment;
         // Add more print statements for other properties if needed
       });
     } catch (e) {
@@ -258,64 +240,63 @@ class CurriculumScreenState extends State<CurriculumScreen> {
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-          if (index == 0) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          }
-          if (index == 1) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => MyCourseCompletedPage()),
-            );
-          }
-          if (index == 2) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => IndoxChatsPage()),
-            );
-          }
-          if (index == 3) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => TransactionsPage()),
-            );
-          }
-          if (index == 4) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => ProfilesPage()),
-            );
-          }
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'My Courses',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Inbox',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.wallet),
-            label: 'Transaction',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        selectedFontSize: 12,
-        selectedLabelStyle: CustomTextStyles.labelLargeGray700,
-        selectedItemColor: Color(0xbbff9300),
-        unselectedItemColor: Color(0xffff9300),
-      ),
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+            if (index == 0) {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            }
+            if (index == 1) {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (context) => MyCourseCompletedPage()),
+              );
+            }
+            if (index == 2) {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => IndoxChatsPage()),
+              );
+            }
+            if (index == 3) {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => TransactionsPage()),
+              );
+            }
+            if (index == 4) {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => ProfilesPage()),
+              );
+            }
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'My Courses',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Inbox',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wallet),
+              label: 'Transaction',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Color(0xbbff9300),
+          unselectedItemColor: Color(0xffff9300),
+        ),
       ),
     );
   }
@@ -580,7 +561,7 @@ class CurriculumScreenState extends State<CurriculumScreen> {
                   Expanded(
                     child: TextButton(
                       onPressed: () async {
-                        if(moduleUndoAssignmentAttempt[moduleAssignmentMap[module
+                        if(moduleUngradeAssignmentAttempt[moduleAssignmentMap[module
                             .id.toString()]![assignmentIndex].id]?.isEmpty ?? false){
                           await Navigator.push(
                             context,
@@ -594,28 +575,29 @@ class CurriculumScreenState extends State<CurriculumScreen> {
                                             minutes: moduleAssignmentMap[
                                             module.id
                                                 .toString()]![assignmentIndex]
-                                                .deadline as int), isOnlineClass: false,)),
+                                                .deadline as int))),
                           );
+                          loadAssignmentAttemptByAssignmentId(moduleAssignmentMap[module
+                              .id.toString()]![assignmentIndex].id
+                              .toString());
+                          loadAssignmentAttemptsByLearnerId();
                         }
-                        if(moduleUndoAssignmentAttempt[moduleAssignmentMap[module
-                            .id.toString()]![assignmentIndex].id]?.isNotEmpty ?? false) {
-                            await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      ReviewAssignment(
-                                          assignmentID: moduleAssignmentMap[module
-                                              .id.toString()]![assignmentIndex].id
-                                              .toString(),
-                                        isOnlineClass: false,
-                                          )),
-                            );
-
-                          }
+                        if(moduleUngradeAssignmentAttempt[moduleAssignmentMap[module
+                            .id.toString()]![assignmentIndex].id]?.isNotEmpty ?? true){
+                        await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                        builder: (context) =>
+                        ViewAllAssignmentAttempt(
+                        assignmentId: moduleAssignmentMap[module
+                            .id.toString()]![assignmentIndex].id
+                            .toString(), navigateTime: 1,)));
                         loadAssignmentAttemptByAssignmentId(moduleAssignmentMap[module
                             .id.toString()]![assignmentIndex].id
                             .toString());
                         loadAssignmentAttemptsByLearnerId();
+                        }
+
                       },
                       child:
                       Html(
